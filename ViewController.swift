a072b4f5d0a507d539b9b208dd2a9e91d002f577
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-10 10:10:59 -0600

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoRecorder
+//
+//  Created by Mitchell Budge on 7/10/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
