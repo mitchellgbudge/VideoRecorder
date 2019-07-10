249a8a19a5431731118a9f0f269e09b8ba5b814d
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-10 11:38:40 -0600

    Add camera preview functionality

--- CameraViewController.swift
+++ CameraViewController.swift
@@ -7,10 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
     
+    lazy private var captureSession = AVCaptureSession()
+    
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var cameraView: CameraPreviewView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let camera = bestCamera()
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            fatalError()
+        }
+        if captureSession.canAddInput(cameraInput) {
+            captureSession.addInput(cameraInput)
+        }
+        
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.canSetSessionPreset(.hd1920x1080)
+        }
+        
+        captureSession.commitConfiguration()
+        
+        cameraView.session = captureSession
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
+    }
+    
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        fatalError("No cameras exist - you're probably running on the simulator")
+    }
     
     @IBAction func recordButtonPressed(_ sender: Any) {
         
