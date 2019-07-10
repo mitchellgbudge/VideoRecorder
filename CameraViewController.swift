504b5af1d241f88264d23d0ee57ce4efd0cdc2f7
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-10 10:27:31 -0600

    Configure UI for camera

---
+++ CameraViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CameraViewController.swift
+//  VideoRecorder
+//
+//  Created by Mitchell Budge on 7/10/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+    
+}
+
--- ViewController.swift
+++ ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showCamera()
     }
 
+    private func showCamera() {
+        performSegue(withIdentifier: "ShowCamera", sender: self)
+    }
 
 }
 
